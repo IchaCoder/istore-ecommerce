4f34a95475c188d0e274801be438e66ff619ed68
Emmanuel Yeboah <ichabordyeboah247@gmail.com>
2022-02-25 07:57:22 +0000

    added cart logo on navbar

--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -6,6 +6,7 @@ import { showModal } from "../redux/actions";
 import { useDispatch } from "react-redux";
 import { CgMenuGridR } from "react-icons/cg";
 import { Link } from "react-router-dom";
+import { AiOutlineShoppingCart } from "react-icons/all";
 
 const Nav = () => {
 	const [scrollPosition, setScrollPosition] = useState(0);
@@ -63,15 +64,23 @@ const Nav = () => {
 					</Link>
 				</ul>
 			</div>
-			<button
-				className={`${
-					scrollPosition > 150
-						? `hover:bg-primary hover:text-white sticky top-0 left-0`
-						: ""
-				} px-4 uppercase outline rounded-lg hover:bg-white hover:text-primary transition-all duration-500`}
-			>
-				log in
-			</button>
+			<div className="flex gap-4">
+				<div className="cursor-pointer relative mt-2">
+					<div className="absolute h-3 w-3 grid place-items-center -top-1 rounded-md text-xs bg-blue-700 -right-1">
+						1
+					</div>
+					<AiOutlineShoppingCart className="w-5 h-5" />
+				</div>
+				<button
+					className={`${
+						scrollPosition > 150
+							? `hover:bg-primary hover:text-white sticky top-0 left-0`
+							: ""
+					} px-4 uppercase outline rounded-lg hover:bg-white hover:text-primary transition-all duration-500`}
+				>
+					log in
+				</button>
+			</div>
 			<Overlay />
 		</nav>
 	);
